b2cd11dd3786c8415d1410d3a5053f4ccb5767ac
Cristian Rosu <redy_kdwa@yahoo.com>
2013-11-23 13:53:42 +0200

    Added new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PublicScreen::Application.config.secret_token = '2c1ab38d316126baddd42606cc2ce3bc86e3aa8296dcad2eb0994e30b67812afc07e5c53b01f9e2554f4841e8dd2ec55f5ee2c6081d222d88c3ab740e16eef2f'
